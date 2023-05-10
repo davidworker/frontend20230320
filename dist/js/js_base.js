b8a056b1f81392b833959c70d6d74f0139022b73
kindpingwork <kindpingwork@gmail.com>
2023-05-10 21:19:19 +0800

    JS 基礎

---
+++ dist/js/js_base.js
@@ -0,0 +1,8 @@
+let age = 18;
+let firstName = 'David';
+let lastName = 'Lin';
+let country = 'TW';
+
+console.log('(' + country + ') ' + firstName + ' ' + lastName);
+
+console.log(`(${country}) ${firstName} ${lastName}`);
