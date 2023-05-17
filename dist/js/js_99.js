b276ba0339a8c1a98702703b9369f07cbc1c1326
kindpingwork <kindpingwork@gmail.com>
2023-05-17 20:27:29 +0800

    抓取n1 n2

--- dist/js/js_99.js
+++ dist/js/js_99.js
@@ -1,5 +1,9 @@
 const genTable = () => {
-    console.log('123');
+    let n1 = document.querySelector('#n1');
+    let n2 = document.querySelector('#n2');
+
+    console.log(n1, n2);
+    console.log(n1.value, n2.value);
 }
 
 
