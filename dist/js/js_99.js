52e979a3ea04e30ea08f9b6e017a916242a7dd25
kindpingwork <kindpingwork@gmail.com>
2023-05-17 19:59:56 +0800

    99 乘法表 UI Layout

--- dist/js/js_99.js
+++
@@ -1,9 +0,0 @@
-let main = document.querySelector('#main');
-let lis = document.querySelectorAll('#menu li');
-
-let num1 = document.querySelector('#num-1');
-let showBtn = document.querySelector('#show-btn');
-
-showBtn.addEventListener('click', () => {
-    num1.value = 'ABC';
-});
