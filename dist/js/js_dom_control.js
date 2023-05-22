b010efa320026a0ee1f299da5e2dc43203a172a8
kindpingwork <kindpingwork@gmail.com>
2023-05-22 21:07:09 +0800

    js class 操控

--- dist/js/js_dom_control.js
+++ dist/js/js_dom_control.js
@@ -7,3 +7,15 @@ doing.addEventListener('click', () => {
     uid.style.fontSize = '100px';
     uid.style.backgroundColor = 'yellow';
 });
+
+let addClass = document.querySelector('#add-class');
+
+addClass.addEventListener('click', () => {
+    uid.classList.add('active');
+});
+
+let removeClass = document.querySelector('#remove-class');
+
+removeClass.addEventListener('click', () => {
+    uid.classList.remove('active');
+});
