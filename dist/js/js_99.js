5f3a60e0fc23012f794111c5d90b26028bd8f4e9
kindpingwork <kindpingwork@gmail.com>
2023-05-22 20:31:56 +0800

    乘法表完成

--- dist/js/js_99.js
+++ dist/js/js_99.js
@@ -33,6 +33,10 @@ const genTable = () => {
     let thead = table.querySelector('thead');
     let tbody = table.querySelector('tbody');
 
+    // 初始化 UI
+    thead.innerHTML = '';
+    tbody.innerHTML = '';
+
     // 初始化 thead UI
     // HTML 內容初始化
     let theadHTML = '<tr><th></th>';
@@ -69,51 +73,7 @@ const genTable = () => {
         tbodyHTML += '</tr>';
         tbody.innerHTML += tbodyHTML;
     }
-
-    console.log(result);
-
-
-    return;
-
-
-
-
-    // let result = {};
-    for (let i = 1; i <= n1.value; i++) {
-        thead.push(i);
-        result[i] = [];
-        for (let j = 1; j <= n2.value; j++) {
-            result[i].push(i * j);
-        }
-    }
-
-    // let table = document.querySelector('#table');
-    let theadTr = table.querySelector('thead tr');
-
-    theadTr.innerHTML = '';
-    thead.forEach((n, nindex) => {
-        theadTr.innerHTML += `<th>${n}</th>`;
-        // theadTr.innerHTML = theadTr.innerHTML + `<th>${n}</th>`;
-    });
-
-
-    // let tbody = table.querySelector('tbody');
-    let tbodyTr = '';
-    tbody.innerHTML = '';
-
-    for (let row in result) {
-        tbodyTr = `<tr><td>${row}</td>`;
-        let columns = result[row];
-        columns.forEach(value => {
-            tbodyTr += `<td>${value}</td>`;
-        });
-
-        tbodyTr += '</tr>';
-        tbody.innerHTML += tbodyTr;
-    }
 }
-
-
 
 let make = document.querySelector('#make');
 
