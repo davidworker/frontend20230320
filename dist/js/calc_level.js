49b956e1f4949d5944e6b823fe85b68984d6a4b1
kindpingwork <kindpingwork@gmail.com>
2023-05-29 19:07:34 +0800

    分數等級轉換邏輯

--- dist/js/calc_level.js
+++ dist/js/calc_level.js
@@ -1,3 +1,26 @@
+const toLevel = (number) => {
+    // >= 90 甲
+    if (number >= 90) {
+        return '甲';
+    }
+    // >= 80 乙
+    if (number >= 80) {
+        return '乙';
+    }
+    // >= 70 丙
+    if (number >= 70) {
+        return '丙';
+    }
+    // >= 60 丁
+    if (number >= 60) {
+        return '丁';
+    }
+
+    // < 60 不及格
+    return '不及格';
+}
+
+
 const calcLevel = () => {
     let number = document.querySelector('#number');
 
@@ -13,7 +36,7 @@ const calcLevel = () => {
 
     let response = document.querySelector('#response');
     // 修正為計算等級
-    response.innerHTML = `Hi!, ${number.value}`;
+    response.innerHTML = `您的等級為: ${toLevel(number.value)}`;
     number.value = '';
     number.focus();
 }
