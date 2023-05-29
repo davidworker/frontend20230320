4565257876b279c834b34373e484c23e7a836d9a
kindpingwork <kindpingwork@gmail.com>
2023-05-29 18:37:20 +0800

    初始化 calc 練習

---
+++ dist/js/calc_level.js
@@ -0,0 +1,47 @@
+const sayHi = () => {
+    let name = document.querySelector('#name');
+
+    if (!name) {
+        alert('沒有輸入框');
+        return;
+    }
+
+    if (!name.value) {
+        alert('請輸入姓名');
+        return
+    }
+
+    let response = document.querySelector('#response');
+    response.innerHTML = `Hi!, ${name.value}`;
+    name.value = '';
+    name.focus();
+}
+
+
+let hi = document.querySelector('#hi');
+
+hi.addEventListener('click', sayHi);
+
+
+let name = document.querySelector('#name');
+
+// name.addEventListener('change', () => {
+//     if (!name.value) {
+//         alert('請輸入姓名');
+//         name.focus();
+//     }
+// })
+
+// name.addEventListener('blur', () => {
+//     console.log(`blur: ${name.value}`);
+// })
+
+// name.addEventListener('keypress', () => {
+//     console.log(`keypress: ${name.value}`);
+// })
+
+name.addEventListener('keyup', (e) => {
+    if (e.key == 'Enter') {
+        sayHi();
+    }
+})
