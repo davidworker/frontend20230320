5677e6c27b4c023eb076ed0ad2f63f36d41e176c
kindpingwork <kindpingwork@gmail.com>
2023-06-05 20:50:46 +0800

    get 增加預設值參數 修正無 key 時發生型態錯誤

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -3,12 +3,13 @@ const database = {
         value = JSON.stringify(value);
         localStorage.setItem(key, value);
     },
-    get(key) {
+    get(key, def) {
         let value = localStorage.getItem(key);
         if (value) {
             value = JSON.parse(value);
+            return value;
         }
-        return value;
+        return def;
     },
     remove(key) {
         localStorage.removeItem(key);
@@ -55,6 +56,6 @@ let vm = Vue.createApp({
         }
     },
     mounted() {
-        this.pending = database.get('todo-pending');
+        this.pending = database.get('todo-pending', []);
     }
 }).mount('#app');
